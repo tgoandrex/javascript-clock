fc1fa1ef738b85f4adabd9996bffe14fd0f58181
tgoandrex <tgoandrex@gmail.com>
2023-08-28 18:40:58 -0500

    Adjust buildClock for Analog to properly work. Add analogBtn to digital class and digitalBtn to analog class. Add analog-clock.jpg for analog clock. Typescript config changes. Other minor adjustments

--- webpack.common.js
+++ webpack.common.js
@@ -18,10 +18,6 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
             }
         ]
     },
